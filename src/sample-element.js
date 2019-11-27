c2419e69e05fa50a0623547bc1d52119b5d63e41
piyoppi <garakuta.toolbox@gmail.com>
2019-11-27 23:20:49 +0900

    プロパティをつかってみる

--- src/sample-element.js
+++ src/sample-element.js
@@ -1,9 +1,20 @@
 import { LitElement, html } from 'lit-element';
 
 class SampleElement extends LitElement {
+  constructor() {
+    super();
+    this.name = 'default name';
+  }
+
+  static get properties() {
+    return {
+      name: { type: String },
+    };
+  }
+
   render(){
     return html`
-      <p>Sample</p>
+      <p>${this.name}</p>
     `;
   }
 }
